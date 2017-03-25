d1ca8bb51ccf5d7da1067699af1d2dbb851166fb
jenward <jenforce@aol.com>
2017-03-25 00:54:06 -0400

    add node server and handlebars

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var app = express();
+
+
+console.log('process.env.PORT', process.env.PORT);
+var port = process.env.PORT || 3010;
+
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//***********Routes to Handlebars TravisG**********//
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/menu', function(req, res) {
+  res.render('menu');
+});
+
+app.get('/kitchen', function(req, res) {
+  res.render('kitchen');
+});
+
+app.get('/sandwich', function(req, res) {
+  res.render('sandwich');
+});
+
+
+app.listen(port, function() {
+    console.log('connected to port ', port);
+});
