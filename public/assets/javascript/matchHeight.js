7713dc94ab7ce71f071f699462b7f3901c12c836
Coffeeteer <tgrieshiemer@gmail.com>
2017-03-26 23:32:51 -0400

    fixed the jQuery connection on handlebars, and made active links on navbar

--- public/assets/javascript/matchHeight.js
+++ public/assets/javascript/matchHeight.js
@@ -1,4 +1,10 @@
 $(function() {
     $('.item').matchHeight(false);
-    $('body').css('background', 'yellow');
+    
+    //Make links '.active' in Handlebars
+    var url = window.location.href;
+      $('nav li').find('.active').removeClass('active');
+      $('nav li a').filter(function(){
+          return this.href == url;
+      }).parent().addClass('active');
 });
